cb2c2b0d44a48f865e650e970c50ea1e939b69ce
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2023-10-20 14:29:38 +0530

    Update Permutations.java

--- Permutations.java
+++ Permutations.java
@@ -1,1 +1,27 @@
+Example of BackTracking
+public class permutations {
+    static void Permutation(String str, String res) {
+        if (str.length() == 0) {
+            System.out.println(res + "");
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String ros = str.substring(0, i) + str.substring(i + 1);
+            Permutation(ros, res + ch);
+        }
+    }
 
+    public static void main(String[] args) {
+        Permutation("ABC", "");
+
+    }
+}
+
+
+Output
+ABC
+ACB
+BAC
+BCA
+CAB
+CBA
