a3beb248986842da6d3a55caf8d0abafddea3b2b
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2023-10-27 18:51:40 +0530

    Update parenthesis.java

--- parenthesis.java
+++ parenthesis.java
@@ -1,1 +1,49 @@
+import java.util.*;
+class Parenthesis{
+	
+		static Boolean Validp(String s){
+			char[] str = new char[s.length()];
+			int top =-1;
+			for(char c :s.toCharArray()){
+				System.out.println(c);
+				if (c == '(' || c == '{' || c == '[' ){ //||c == ')' || c == '}' || c == ']'
+					str[++top]=c;
+				}else if(c == '}' && top >=0 && str[top]=='{'){
+					top--;
+				} else if(c==')' && top>= 0 &&str[top]=='('){
+					top--;
+				}else if(c == ']' && top>=0 &&str[top]=='['){
+					top--;
+				}
+				else{
+					return false;
+				}
+			}
+			
+			 return top == -1;
+		}
+	/*
+	{{}}
+	
+{[}}
+	
+	*/
+public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	String input = sc.nextLine();
+	//boolean Check = Validp(input);
+	if(Validp(input)){
+		System.out.print("valid");
+	}else{
+		System.out.print("not  valid");
+	}
+	sc.close();
 
+}
+}
+/*
+Check Condition {{}} valid
+	{{}} invalid
+	((()))
+not  valid
+	*/
