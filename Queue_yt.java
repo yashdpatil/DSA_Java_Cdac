ed7f2da6edc743cf00205c82689bd88ab75de414
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2023-10-28 20:31:04 +0530

    Update Queue_yt.java

--- Queue_yt.java
+++ Queue_yt.java
@@ -1,1 +1,63 @@
+class queue_LL{
+	Node rear;
+	Node front;
+	int length =0;
+	
+		queue_LL(){
+			this.rear =null;
+			this.front =null;
+		}
+	public static class Node{
+		int data;
+		Node next;
+		public Node(int data){
+			this.data = data;
+			this.next=null;
+			
+		}
+	
+	}
+	boolean isEmpty(){
+		return length==0;
+	}
+	
+		boolean Enque(int data){
+			Node temp = new Node(data);
+			if(isEmpty()){
+				front=temp;
+			}
+			else{
+				rear.next=temp;
+			}
+			rear = temp;
+			length++;
+			return true;
+		}
+	
+		int Dequeue(){
+			int result = front.data;
+			front = front.next;
+			while(front==null){
+				rear =null;
+			}
+			length--;
+			return result;
+		}
+		void Display(){
+	while(front!=null){
+		System.out.print( front.data+" ");
+		front=front.next;
+	}
+		}
 
+	
+
+
+public static void main(String args[]){
+queue_LL q = new queue_LL();
+q.Enque(10);
+q.Enque(20);
+q.Dequeue();
+q.Display();
+}
+}
