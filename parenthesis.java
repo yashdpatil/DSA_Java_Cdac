a45f92110b9c8c1685e5222f80cd7bb191101417
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2023-11-23 23:39:03 +0530

    Update parenthesis.java
    with new Leetcode code of para...

--- parenthesis.java
+++ parenthesis.java
@@ -40,6 +40,38 @@ public static void main(String args[]){
 
 }
 }
+
+
+
+
+class Solution {
+    public boolean isValid(String s) {
+         Stack<Character> stack = new Stack<>();
+
+        for (char x : s.toCharArray()) {
+
+            if(x=='('){
+                
+                stack.push(')');
+            } else if (x=='{') {
+                stack.push('}');
+                
+            } else if (x=='[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop()!=x) {
+
+                return false;
+            }
+
+        }
+
+
+        return stack.isEmpty();
+        
+    }
+}
+
+
 /*
 Check Condition {{}} valid
 	{{}} invalid
